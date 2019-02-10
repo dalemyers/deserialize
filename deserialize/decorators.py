2cb32f2efc90869ca4173c6cfb5137911776741a
Dale Myers <dale@myers.io>
2019-02-10 16:52:34 +0000

    Add support for ignoring properties

--- deserialize/decorators.py
+++ deserialize/decorators.py
@@ -1,5 +1,31 @@
 """Decorators used for adding functionality to the library."""
 
+def ignore(property_name):
+    """A decorator function for marking keys as those which should be ignored."""
+
+    def store_key_map(class_reference):
+        """Store the key map."""
+        try:
+            _ = class_reference.__deserialize_ignore_map__
+        except AttributeError:
+            setattr(class_reference, "__deserialize_ignore_map__", {})
+
+        class_reference.__deserialize_ignore_map__[property_name] = True
+
+        return class_reference
+
+    return store_key_map
+
+
+def _should_ignore(class_reference, property_name):
+    """Check if a property should be ignored."""
+
+    try:
+        return class_reference.__deserialize_ignore_map__.get(property_name, False)
+    except AttributeError:
+        return False
+
+
 def key(property_name, key_name):
     """A decorator function for mapping key names to properties."""
 
