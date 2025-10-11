13a8bfea5d9a2874912ef3190908bae1bf0c8ab1
Dale Myers <dalemyers@microsoft.com>
2025-10-11 18:22:20 +0100

    Fix linter issue

--- deserialize/custom_deserializable.py
+++ deserialize/custom_deserializable.py
@@ -1,11 +1,12 @@
-"""Handlers for case conversions for strings."""
+"""Protocol for custom deserialization."""
 
 from typing import Any, Protocol, runtime_checkable
 
 
 @runtime_checkable
 class CustomDeserializable(Protocol):
+    """Protocol for classes that implement custom deserialization logic."""
+
     @classmethod
     def deserialize(cls, value: Any) -> "CustomDeserializable":
         """Override this method to implement custom deserialization logic."""
-        ...
